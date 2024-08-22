1788373f3fcd1e0d235c4b48cf9c0df2ccce2158
reedrya <157441470+reedrya@users.noreply.github.com>
2024-08-21 20:37:24 -0700

    Update page.js

--- app/result/page.js
+++ app/result/page.js
@@ -1,3 +1,4 @@
+"use client";
 import { useState, useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { Container, Box, Typography, CircularProgress } from '@mui/material';
